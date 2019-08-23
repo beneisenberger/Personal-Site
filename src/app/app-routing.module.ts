4405acbd4f8b379d6c82d1b71ca924e309ed6bd4
Ben Eisenberger <ben.eisenberger@gmail.com>
2019-08-23 02:01:43 -0500

    added initial layout/some funcionality - blog and web dev empty

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { MediaComponent } from './media/media.component';
+import { EventsComponent } from './events/events.component';
+import { CodeComponent } from './code/code.component';
+import { ContactComponent } from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: 'media', component: MediaComponent},
+  {path: 'events', component: EventsComponent},
+  {path: 'code', component: CodeComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', redirectTo: '/about'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { MediaComponent } from './media/media.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
+import { EventsComponent } from './events/events.component';
+import { MenuComponent } from './menu/menu.component';
+import { CodeComponent } from './code/code.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    MediaComponent,
+    ContactComponent,
+    BlogComponent,
+    EventsComponent,
+    MenuComponent,
+    CodeComponent
   ],
   imports: [
     BrowserModule,
