23c19438e47977e4fd38a0caeda4093a20c44d95
Ben Eisenberger <appsky@Bens-MacBook-Pro.local>
2020-07-16 00:19:18 -0500

    theming some more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { MediaComponent } from './media/media.component';
-import { ContactComponent } from './contact/contact.component';
-import { BlogComponent } from './blog/blog.component';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AboutComponent } from "./about/about.component";
+import { MediaComponent } from "./media/media.component";
+import { ContactComponent } from "./contact/contact.component";
+import { BlogComponent } from "./blog/blog.component";
+import { TooltipModule } from "ngx-bootstrap/tooltip";
+import { CollapseModule, BsDropdownModule } from "ngx-bootstrap";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MDBBootstrapModule } from "angular-bootstrap-md";
 
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireStorageModule } from 'angularfire2/storage';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireModule } from "angularfire2";
+import { AngularFirestoreModule } from "angularfire2/firestore";
+import { AngularFireStorageModule } from "angularfire2/storage";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
-import { environment } from '../environments/environment';
+import { environment } from "../environments/environment";
 
-import { EventsComponent } from './events/events.component';
-import { MenuComponent } from './menu/menu.component';
-import { CodeComponent } from './code/code.component';
-import { LoginComponent } from './login/login.component';
-import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
-import { PostsModule } from './posts/posts.module';
-import { Routes, RouterModule } from '@angular/router';
-import { PostListComponent } from './posts/post-list/post-list.component';
-import { MaterialModule } from './material.module';
-import { FormatDatePipe } from './format-date.pipe';
+import { EventsComponent } from "./events/events.component";
+import { MenuComponent } from "./menu/menu.component";
+import { FooterComponent } from "./menu/footer/footer.component";
+import { CodeComponent } from "./code/code.component";
+import { LoginComponent } from "./login/login.component";
+import { CoreModule } from "./core/core.module";
+import { SharedModule } from "./shared/shared.module";
+import { PostsModule } from "./posts/posts.module";
+import { Routes, RouterModule } from "@angular/router";
+import { PostListComponent } from "./posts/post-list/post-list.component";
+import { MaterialModule } from "./material.module";
+import { FormatDatePipe } from "./format-date.pipe";
 // import { PostDetailComponent } from './posts/post-detail/post-detail.component';
 // import { PostDashboardComponent } from './posts/post-dashboard/post-dashboard.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/blog', pathMatch: 'full'},
-  { path: '', loadChildren: './posts/posts.module#PostsModule'},
-]
+  { path: "", redirectTo: "/blog", pathMatch: "full" },
+  { path: "", loadChildren: "./posts/posts.module#PostsModule" },
+];
 
 @NgModule({
   declarations: [
@@ -48,6 +49,7 @@ const routes: Routes = [
     BlogComponent,
     EventsComponent,
     MenuComponent,
+    FooterComponent,
     CodeComponent,
     LoginComponent,
     // PostListComponent,
@@ -72,9 +74,9 @@ const routes: Routes = [
     SharedModule,
     PostsModule,
     TooltipModule.forRoot(),
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
